b27b1e96e7253afdd85382516d31c9e536ca0bf2
Dariojz <dzubaray@gmail.com>
2020-04-29 16:33:20 -0300

    implementando el resolver para la mutation de agregar un nuevo curso

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -3,7 +3,26 @@ import { database } from '../data/data.store';
 import _ from 'lodash';
 
 const mutation: IResolvers = {
-    
+    Mutation: {
+        cursoNuevo(__:void, {curso}): any {
+
+            const itemCurso = {
+                id: String(database.cursos.length + 1),
+                ...curso
+            }
+
+            if(database.cursos.filter(item => item.title === itemCurso.title).length === 0 ) {
+                database.cursos.push(itemCurso);
+                return itemCurso;
+            }
+
+            return {
+                id: '-1',
+                ...curso,
+                title: `Ya existe un curso con este nombre ${curso.title}`
+            }
+        }
+    }
 }
 
 export default mutation;
