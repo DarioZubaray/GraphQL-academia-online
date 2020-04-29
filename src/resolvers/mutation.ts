c7fa2cbc2fd4622252071ae6a7acff772f187317
Dariojz <dzubaray@gmail.com>
2020-04-29 17:25:08 -0300

    agregando las mutaciones para modificiar y borrar

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -21,6 +21,46 @@ const mutation: IResolvers = {
                 ...curso,
                 title: `Ya existe un curso con este nombre ${curso.title}`
             }
+        },
+        modificarCurso(__:void, {curso}): any {
+            const elementoExiste = _.findIndex(database.cursos, function(o) {
+                return o.id === curso.id;
+            });
+
+            if(elementoExiste > -1) {
+                const valoraciones = database.cursos[elementoExiste].reviews;
+                curso.reviews = valoraciones;
+                database.cursos[elementoExiste] = curso;
+                return curso;
+            }
+
+            return {
+                id: '-1',
+                ...curso,
+                title: `No existe curso ha modificar. ID: ${curso.id}`
+            }
+        },
+        eliminarCurso(__:void, {id}) {
+            const borrarCurso = _.remove(database.cursos, function(o) {
+                return o.id === id;
+            });
+
+            if(!borrarCurso[0]) {
+                return {
+                    id: '-1',
+                    title: `No existe el curso ha borrar. ID: ${id}`,
+                    description: '',
+                    clases: 0,
+                    time: 0.0,
+                    level: "SIN_NIVEL",
+                    logo: '',
+                    path: '',
+                    teacher: '',
+                    reviews: []
+                }
+            }
+
+            return borrarCurso[0];
         }
     }
 }
