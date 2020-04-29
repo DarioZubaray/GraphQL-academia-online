950adabb192ee67d8be643b162918f5a4286fded
Dariojz <dzubaray@gmail.com>
2020-04-29 13:09:20 -0300

    agregando a las queries la busqueda de estudiante por ID Lista de cursos y curso por ID

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -3,8 +3,42 @@ import { database } from '../data/data.store';
 
 const query: IResolvers = {
     Query: {
+        estudiante(__:void, {id}): any {
+            const resultado = database.estudiantes.filter(e => e.id === id)[0];
+            if( resultado === undefined ) {
+                return {
+                    id: '-1',
+                    name: `No se ha encontrado con el id: ${id}`,
+                    email: '',
+                    courses: []
+                }
+            }
+            return resultado;
+        },
         estudiantes(): any {
             return database.estudiantes;
+        },
+        curso(__:void, {id}): any {
+            const resultado = database.cursos.filter(c => c.id === id) [0];
+            if( resultado === undefined) {
+                return {
+                    id: '-1',
+                    title: `No se ha encontrado el curso con el ID: ${id}`,
+                    description: 'Error al buscar el curso solicitado',
+                    clases: 0,
+                    time: 0.0,
+                    logo: '',
+                    level: 'TODOS',
+                    path: '',
+                    teacher: '',
+                    students: [],
+                    reviews: []
+                }
+            }
+            return resultado;
+        },
+        cursos(): any {
+            return database.cursos;
         }
     }
 }
