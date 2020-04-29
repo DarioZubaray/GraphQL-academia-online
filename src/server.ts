4dfbc5d46e800a34e382b1312b33ec994de35b99
Dariojz <dzubaray@gmail.com>
2020-04-29 12:42:12 -0300

    Añadiendo resolvers y configurando el servidor apollo

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,25 @@
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+import schema from './schema';
 import { createServer } from 'http';
+import expressPlayGround from 'graphql-playground-middleware-express';
 
 const app = express();
 app.use('*', cors());
 app.use(compression());
 
-app.get('/', (req, res) => {
-    res.send("Bienvenidos a la academia online");
+const servidor = new ApolloServer({
+    schema,
+    introspection: true
 });
+
+servidor.applyMiddleware({app});
+
+app.get('/', expressPlayGround({
+    endpoint: '/graphql'
+}));
 
 const httpServer = createServer(app);
 
@@ -18,5 +28,5 @@ httpServer.listen(
     {
         port: PORT
     },
-    () => console.log(`Hola juli desde http://localhost:${PORT}`)
+    () => console.log(`Hola juli desde http://localhost:${PORT}/graphql`)
 );
